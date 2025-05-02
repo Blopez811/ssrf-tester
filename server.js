a769e06962567225f11775aa74bcd9b65dbd6166
Benicio Lopez <beniciolopez@benicios-mbp.lan>
2025-05-02 00:28:06 -0500

    added more exploitable endpoints to test out

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+const { exec } = require('child_process');
+const fs = require('fs');
+const dns = require('dns');
 
 PORT = process.env.PORT || 3001
 app.use(express.json());
@@ -23,6 +26,51 @@ app.post('/fetch', async (req, res) => {
     res.status(500).send(err.toString());
   }
 });
+
+app.post('/exec', (req, res) => {
+  const { cmd } = req.body;
+  console.log(`Executing command: ${cmd}`);
+  exec(cmd, (err, stdout, stderr) => {
+    if (err) {
+      console.error(`Error: ${err.message}`);
+      return res.status(500).send(err.message);
+    }
+    res.send({
+      stdout,
+      stderr
+    });
+  });
+});
+
+app.get('/env', (req, res) => {
+  res.send(process.env);
+});
+
+app.post('/dns', (req, res) => {
+  const { hostname } = req.body;
+  dns.lookup(hostname, (err, address, family) => {
+    if (err) return res.status(500).send(err.toString());
+    res.send({ address, family });
+  });
+});
+
+app.post('/read', (req, res) => {
+  const { path } = req.body;
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) return res.status(500).send(err.toString());
+    res.send(data);
+  });
+});
+
+app.post('/ping', (req, res) => {
+  const { target } = req.body;
+  exec(`ping -c 2 ${target}`, (err, stdout, stderr) => {
+    if (err) return res.status(500).send(stderr);
+    res.send(stdout);
+  });
+});
+
+
 
 try{
   app.listen(3001, () => console.log(`SSRF Tester running on port ${PORT}`));
